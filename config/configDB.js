24d498df5bbd05d761ced19c619c3c62e303f97e
noam shamgar <noamshpam12@gmail.com>
2022-02-21 02:36:10 +0200

    bug fixes

--- config/configDB.js
+++ config/configDB.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://noam:123456@cinemacluster.vnjed.mongodb.net/subscriptionsDB?retryWrites=true&w=majority",
+mongoose.connect(`mongodb+srv://${process.env.MONGO_NAME}:${process.env.MONGO_PASS}@cinemacluster.vnjed.mongodb.net/subscriptionsDB?retryWrites=true&w=majority`,
     ()=>{
         console.log("   successfully connected to subscriptions database");
         require("../helpers/fillDB") // filling DB if its empty
